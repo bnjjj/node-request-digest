41ea93eeae8fe2c75c96c174457392583e8f3584
Benjamin Coenen <benjamin.coenen@hotmail.com>
2016-03-04 11:41:57 +0100

    tests (e2e): add new tests with promise

--- tests/e2e.js
+++ tests/e2e.js
@@ -1,8 +1,9 @@
 'use strict';
 var requestDigest = require('../request-digest.es5')(process.env.USER, process.env.PASSWORD);
+var expect = require('chai').expect;
 
 describe('+ End to end tests : ', function () {
-  it('- Test with GET HTTP : ', function (done) {
+  it('- Test with GET HTTP Callback: ', function (done) {
     requestDigest.request({
       host: process.env.URL,
       path: process.env.HOST,
@@ -12,6 +13,32 @@ describe('+ End to end tests : ', function () {
         'mail': process.env.MAIL,
         'password': process.env.PASSWORDHEADER
       }
-    }, done);
+    }, function (err, resp, body) {
+      expect(body != null).to.equal(false);
+      expect(err != null).to.equal(true);
+      done();
+    });
+  });
+
+  it('- Test with GET HTTP Promise: ', function (done) {
+    requestDigest.requestAsync({
+      host: process.env.URL,
+      path: process.env.HOST,
+      port: 80,
+      method: 'GET',
+      headers: {
+        'mail': process.env.MAIL,
+        'password': process.env.PASSWORDHEADER
+      }
+    })
+    .then(function (resp) {
+      expect(resp == null).to.equal(true);
+    })
+    .catch(function (err) {
+      expect(err != null).to.equal(true);
+    })
+    .finally(function () {
+      done();
+    });
   });
 });
